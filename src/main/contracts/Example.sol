e5b6ef73005fd382593ee7e1133250a9b17a8cbb
Ben Evans <benjamin.john.evans@gmail.com>
2018-05-08 11:00:32 +0100

    Initial Solidity contracts

---
+++ src/main/contracts/Example.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.19;
+
+contract Example 
+{ 
+    uint256 data; 
+    address owner; 
+
+    event logData(uint256 dataToLog);  
+
+    modifier onlyOwner() { 
+        if (msg.sender != owner) throw; 
+        _; 
+    } 
+
+    // Java-like constructor syntax 
+    // This code is run only when the contract is created.
+    function Example(uint256 initData, address initOwner) public { 
+        data = initData; 
+        owner = initOwner; 
+    } 
+
+    // Accessors
+    function getData() returns (uint256 out){
+        return data; 
+    } 
+
+    function setData(uint256 newData) onlyOwner { 
+        logData(newData); 
+        data = newData; 
+    } 
+}
